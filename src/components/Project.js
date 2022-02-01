b85ceac1dc29e861f5f1ca43bb84d300c5e9edc2
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-01-31 17:13:28 -0700

    Display languages for Project

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,6 +4,13 @@ export default function Project({ project, getIconComponent }) {
   return (
     <div className="project">
       <h3>{project.title}</h3>
+      <div className="langs">
+        {project.langs.map((lang, i) => (
+          <div key={i} className="lang">
+            {getIconComponent(lang)}
+          </div>
+        ))}
+      </div>
       <h5>{project.subtitle}</h5>
       <p>{project.description}</p>
       <div className="links">
